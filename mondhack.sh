eb3f173523a2295b0a30b3648c422fea45728674
Mamankmarcel <57529908+Mamankmarcel@users.noreply.github.com>
2019-11-09 11:07:31 +0700

    Add files via upload

---
+++ mondhack.sh
@@ -0,0 +1,28 @@
+
+#!/bin/sh
+clear
+echo "\033[32;1m"
+echo " _____                             ___ "
+echo "|     \  |    ^       ___  ___    /   \    |\   |"
+echo "|     |  |   / \     |   \/   |   |   |    | \  |" 
+echo "|     |  |  /   \    |        |   |   |    |  \ |"
+echo "|_____/  | /     \   |        |   \___/    |   \|"  
+echo "script diamond MLBB"
+read -p "siapakah username MLBB anda: " username
+read -p "apa id MLBB anda: " id
+echo "mencari username $username dan id $id..  "
+sleep 1
+echo
+echo
+echo "sudah tertemu"
+echo
+echo
+sleep 0.5
+read -p "berapakah diamond yg ingin anda isi: " diamond
+echo "mengisi diamond $diamond ke id $id ..  "
+sleep 5
+echo
+echo "tunggu beberapa menit.. "
+echo "maka diamond akan terisi"
+sleep 2
+exit
